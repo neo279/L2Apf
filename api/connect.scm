1b72c1cc732d87a8d6aaae0685eaf538fe43f66d
filip <62439459+neo279@users.noreply.github.com>
2020-03-28 04:58:59 +0100

    fix: add missing first packet decryption

--- api/connect.scm
+++ api/connect.scm
@@ -18,11 +18,11 @@
 		(connect (->* (string?) (integer? string? integer?) connection?))
 	))
 
-	(define (connect host [port 2106] [token #"_;5.]94-31==-%xT!^[$"] [protocol 660])
+	(define (connect host [port 2106] [token (bytes #x6B #x60 #xCB #x5B #x82 #xCE #x90 #xB1 #xCC #x2B #x6C #x55 #x6C #x6C #x6C #x6C)] [protocol 273])
 		(let ((cn (connection protocol)) (pc (make-async-channel)) (crypter (make-blowfish-crypter token)))
 			(let-values (((input-port output-port) (tcp-connect host port)))
 				(let loop ()
-					(let ((buffer (if (connection-session-id cn) (read-packet cn) (read-buffer input-port))))
+					(let ((buffer (if (connection-session-id cn) (read-packet cn) (read-buffer input-port crypter))))
 						(case (get-packet-id buffer)
 							((#x00) (let ((packet (login-server-packet/init buffer)))
 								(set-connection-packet-channel! cn pc)
